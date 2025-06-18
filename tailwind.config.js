e0d9bc66fca46a6d95d7255e6823e8ad396f615a
joseph <josephpaulk@yahoo.com>
2025-06-18 13:56:36 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+ module.exports = {
   theme: {
-    extend: {},
+    fontSize: {
+      'base': '1.125rem', // 18px instead of 16px
+      'sm': '0.875rem',  // 14px
+      'lg': '1.25rem',   // 20px
+      'xl': '1.5rem',    // 24px
+      '2xl': '1.875rem', // 30px
+      '3xl': '2.25rem',  // 36px
+      '4xl': '3rem',     // 48px
+      '5xl': '3.75rem',  // 60px
+    },
   },
+};,
   plugins: [],
 };
